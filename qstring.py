ef5ce1939027b8cec6ee16ffe0c29e4a33edbc6c
Tomasz Gebarowski <tomasz.gebarowski@azetti.com>
2015-01-19 14:55:55 +0100

    Fixing issue with empty String

--- qstring.py
+++ qstring.py
@@ -5,14 +5,14 @@ def utf16string_summary(value, *rest):
     length = d.GetChildMemberWithName("size").GetValueAsSigned()
     offset = d.GetChildMemberWithName("offset").GetValueAsSigned()
     address = d.GetValueAsUnsigned() + offset
+
+    if length == 0:
+        return '""'
     error = lldb.SBError()
     # UTF-16, so we multiply length by 2
     bytes = value.GetProcess().ReadMemory(address, length * 2, error)
     if bytes is None:
         return '""'
-    if length == 0:
-        return '""'
-
     return '"%s"' % (bytes.decode('utf-16').encode('utf-8'))
 
 def __lldb_init_module(debugger, *rest):
